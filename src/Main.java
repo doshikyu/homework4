a0e00f8fc7b0ba04ff75d420015048004eb3413a
doshikyu <yu.dashik@gmail.com>
2023-09-19 00:12:33 +0900

    - Tasks 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int agePerson1 = 17;
+        int agePerson2 = 117;
+        if (agePerson1 >= 18){
+            System.out.println("Если возраст человека равен " + agePerson1 +", то он совершеннолетний.");
+        } else{
+            System.out.println("Если возраст человека равен " + agePerson1 +", то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+        if (agePerson2 >= 18){
+            System.out.println("Если возраст человека равен " + agePerson2 +", то он совершеннолетний.");
+        } else{
+            System.out.println("Если возраст человека равен " + agePerson2 +", то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+        System.out.println("Задача 2");
+        int temperatureExampleCold = -10;
+        int temperatureExampleWarm = 16;
+        if (temperatureExampleCold < 5){
+            System.out.println("На улице " + temperatureExampleCold + " градусов, нужно надеть шапку.");
+        }
+        if (temperatureExampleCold > 5){
+            System.out.println("На улице " + temperatureExampleCold + " градусов, можно идти без шапки.");
+        }
+        if (temperatureExampleWarm < 5){
+            System.out.println("На улице " + temperatureExampleWarm + " градусов, нужно надеть шапку.");
+        }
+        if (temperatureExampleWarm > 5){
+            System.out.println("На улице " + temperatureExampleWarm + " градусов, можно идти без шапки.");
+        }
+
     }
 }
